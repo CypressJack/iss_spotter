358dda050478ba10e3ec11d4811eb622913d1d9b
archaemedes <wadecroft@gmail.com>
2021-05-06 23:50:10 +0000

    Built everything myself except for the risetime interpreting loop because i wasn't sure what that was

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-
 const { nextISSTimesForMyLocation } = require('./iss');
 
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
-  // success, print out the deets!
-  console.log(passTimes);
+  for (const pass of passTimes){
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
 });
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-// fetchCoordsByIP('24.86.142.101', (error, data)=>{
-//   console.log(data);
-// })
-
-
-// const testCoords = {
-//   longitude: '-123.0961',
-//   latitude: '49.2643'
-// }
-
-
-// fetchISSFlyOverTime(testCoords, (error, data)=>{
-//   console.log(error);
-//   console.log(data)
-// });
-
